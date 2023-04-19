2b5b11996cb8ad262c96bd2389f87e874bd53074
Juan Pablo Tabares <juancho9615@hotmail.com>
2023-04-19 11:40:03 -0500

    Updated Validation

--- src/auth/pages/RegisterPage.tsx
+++ src/auth/pages/RegisterPage.tsx
@@ -2,6 +2,19 @@ import { Link as RouterLink } from 'react-router-dom'
 import { Grid, TextField, Button, Typography, Link } from '@mui/material'
 import { AuthLayout } from '../layout/AuthLayout'
 import { useFormRegister } from '../../hooks/useForm'
+import { useState } from 'react'
+interface Errors {
+  email: boolean,
+  password: boolean,
+  displayName: boolean
+}
+
+const errorsData: Errors = {
+  email: false,
+  password: false,
+  displayName: false
+}
+
 
 const formData = {
   email: '',
@@ -13,13 +26,59 @@ const formData = {
 
 export const RegisterPage = () => {
 
-  const { displayName, email, password, onInputChange, formState, onSubmitForm, errorForm } = useFormRegister(formData);
+  const { displayName, email, password, onInputChange, formState } = useFormRegister(formData);
+  const [errorForm, setErrorForm] = useState(errorsData);
 
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const newErrors: Errors = {
+      email: false,
+      password: false,
+      displayName: false
+    };
+    let hasError = false;
 
+    if (displayName.trim() === '' || password.trim() === '' || email.trim() === '') {
+      newErrors.displayName = displayName.trim() === '';
+      newErrors.email = email.trim() === '';
+      newErrors.password = password.trim() === '';
+      hasError = true;
+
+      setTimeout(() => {
+        setErrorForm({
+          email: false,
+          password: false,
+          displayName: false
+        });
+      }, 3000);
+    } else {
+      if (displayName.trim().length < 1) {
+        newErrors.displayName = true;
+        hasError = true;
+      }
+
+      if (!regex.test(email)) {
+        newErrors.email = true;
+        hasError = true;
+      }
+
+      if (password === '') {
+        newErrors.password = true;
+        hasError = true;
+      }
+    }
+
+    if (!hasError) {
+      console.log(formState);
+    }
+
+    setErrorForm(newErrors);
+  }
   return (
     <AuthLayout title='Crear Cuenta' >
-      <form onSubmit={onSubmitForm} >
+      <form onSubmit={onSubmit} >
         <Grid container>
 
           <Grid item xs={12} sx={{ mt: 2 }}>
--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,14 +1,15 @@
 import { useState } from 'react';
-import { Errors, FormState, FormRegister } from './hooks.type';
 
-
-
-const errorsData: Errors = {
-    email: false,
-    password: false,
-    displayName: false
+interface FormState {
+    email: string;
+    password: string;
 }
 
+interface FormRegister {
+    email: string;
+    password: string;
+    displayName: string;
+}
 
 export const useForm = (initialForm: FormState) => {
 
@@ -35,59 +36,25 @@ export const useForm = (initialForm: FormState) => {
 }
 
 export const useFormRegister = (initialForm: FormRegister) => {
+
     const [formState, setFormState] = useState(initialForm);
-    const [errorForm, setErrorForm] = useState(errorsData);
 
     const onInputChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = target;
         setFormState({
             ...formState,
-            [name]: value,
+            [name]: value
         });
-    };
+    }
 
     const onResetForm = () => {
         setFormState(initialForm);
-    };
-
-    const onSubmitForm = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        const newErrors: Errors = { ...errorsData };
-        let hasError = false;
-
-        if (formState.displayName.trim().length < 1) {
-            newErrors.displayName = true;
-            hasError = true;
-        }
-
-        if (!regex.test(formState.email)) {
-            newErrors.email = true;
-            hasError = true;
-        }
-
-        if (formState.password === "") {
-            newErrors.password = true;
-            hasError = true;
-        }
-
-        setErrorForm(newErrors);
-
-        if (!hasError) {
-            // Aquí iría el código para enviar el formulario si no hay errores
-        }
-
-        setTimeout(() => {
-            setErrorForm(errorsData);
-        }, 2000);
-    };
+    }
 
     return {
         ...formState,
         formState,
         onInputChange,
         onResetForm,
-        onSubmitForm,
-        errorForm,
-    };
-};
+    }
+}
