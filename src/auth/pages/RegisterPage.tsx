498d1bb603b81dba4398d4d6f87edf3fd03de129
Juan Pablo Tabares <juancho9615@hotmail.com>
2023-04-19 11:30:44 -0500

    Agregando Validacion Form

--- src/auth/pages/RegisterPage.tsx
+++ src/auth/pages/RegisterPage.tsx
@@ -3,36 +3,39 @@ import { Grid, TextField, Button, Typography, Link } from '@mui/material'
 import { AuthLayout } from '../layout/AuthLayout'
 import { useFormRegister } from '../../hooks/useForm'
 
-
 const formData = {
-  email: 'juanpablo@google.com',
-  password: '123456',
-  displayName: 'Juan pablo',
+  email: '',
+  password: '',
+  displayName: '',
 }
+
+
 
 export const RegisterPage = () => {
 
-  const { displayName, email, password, onInputChange } = useFormRegister(formData);
+  const { displayName, email, password, onInputChange, formState, onSubmitForm, errorForm } = useFormRegister(formData);
+
+
 
   return (
     <AuthLayout title='Crear Cuenta' >
-      <form >
+      <form onSubmit={onSubmitForm} >
         <Grid container>
 
           <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField label='Nombre Completo' type="text" placeholder="Tu Nombre" fullWidth />
+            <TextField error={errorForm['displayName']} helperText={errorForm['displayName'] && 'El nombre debe ser valido'} value={displayName} name='displayName' onChange={onInputChange} label='Nombre Completo' type="text" placeholder="Tu Nombre" fullWidth />
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField label='Correo' type="email" placeholder="Correo@correo.com" fullWidth />
+            <TextField helperText={errorForm['email'] && 'El email debe ser valido'} error={errorForm['email']} value={email} name='email' onChange={onInputChange} label='Correo' type="email" placeholder="Correo@correo.com" fullWidth />
           </Grid>
 
           <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField label='Contraseña' type="password" placeholder="Contraseña" fullWidth />
+            <TextField helperText={errorForm['password'] && 'El password debe ser valido'} error={errorForm['password']} value={password} name='password' onChange={onInputChange} label='Contraseña' type="password" placeholder="Contraseña" fullWidth />
           </Grid>
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12} sx={{ mt: 2 }}>
-              <Button variant='contained' fullWidth>Crear Cuenta</Button>
+              <Button type='submit' variant='contained' fullWidth>Crear Cuenta</Button>
             </Grid>
           </Grid>
 
--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,15 +1,14 @@
 import { useState } from 'react';
+import { Errors, FormState, FormRegister } from './hooks.type';
 
-interface FormState {
-    email: string;
-    password: string;
+
+
+const errorsData: Errors = {
+    email: false,
+    password: false,
+    displayName: false
 }
 
-interface FormRegister {
-    email: string;
-    password: string;
-    displayName: string;
-}
 
 export const useForm = (initialForm: FormState) => {
 
@@ -36,25 +35,59 @@ export const useForm = (initialForm: FormState) => {
 }
 
 export const useFormRegister = (initialForm: FormRegister) => {
-
     const [formState, setFormState] = useState(initialForm);
+    const [errorForm, setErrorForm] = useState(errorsData);
 
     const onInputChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = target;
         setFormState({
             ...formState,
-            [name]: value
+            [name]: value,
         });
-    }
+    };
 
     const onResetForm = () => {
         setFormState(initialForm);
-    }
+    };
+
+    const onSubmitForm = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const newErrors: Errors = { ...errorsData };
+        let hasError = false;
+
+        if (formState.displayName.trim().length < 1) {
+            newErrors.displayName = true;
+            hasError = true;
+        }
+
+        if (!regex.test(formState.email)) {
+            newErrors.email = true;
+            hasError = true;
+        }
+
+        if (formState.password === "") {
+            newErrors.password = true;
+            hasError = true;
+        }
+
+        setErrorForm(newErrors);
+
+        if (!hasError) {
+            // Aquí iría el código para enviar el formulario si no hay errores
+        }
+
+        setTimeout(() => {
+            setErrorForm(errorsData);
+        }, 2000);
+    };
 
     return {
         ...formState,
         formState,
         onInputChange,
         onResetForm,
-    }
-}
+        onSubmitForm,
+        errorForm,
+    };
+};
